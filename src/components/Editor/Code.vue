742c4b231129fdde720ea0030fd28144833bd6d5
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-26 01:13:50 +0800

    fix: don't save on url (temp)

--- src/components/Editor/Code.vue
+++ src/components/Editor/Code.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { editorValue } from "src/composables/state";
-import { PLAYGROUND_DEMO_CODE } from "src/utils/constants";
-import { urlParamsInst } from "src/utils/url";
 import { ref } from "vue";
 import MonacoEditor from "./Monaco.vue";
 
@@ -13,10 +11,7 @@ const editorContainer = ref(null);
 
 const setValue = (newValue: string) => {
   editorValue.value = newValue;
-  urlParamsInst.updateCode(newValue);
 };
-
-setValue(urlParamsInst.code || PLAYGROUND_DEMO_CODE);
 </script>
 
 <template>
