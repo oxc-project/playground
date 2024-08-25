048bd4ea45ed12a8ab06c4bd8d1962ef97a269f3
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-26 00:31:29 +0800

    fix: improve page layout

--- src/components/Editor/Code.vue
+++ src/components/Editor/Code.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useElementSize } from "@vueuse/core";
 import { editorValue } from "src/composables/state";
 import { PLAYGROUND_DEMO_CODE } from "src/utils";
 import { urlParamsInst } from "src/utils";
@@ -9,7 +8,6 @@ import MonacoEditor from "./Monaco.vue";
 defineProps(["language"]);
 
 const editorContainer = ref(null);
-const { width, height } = useElementSize(editorContainer);
 
 const setValue = (newValue: string) => {
   editorValue.value = newValue;
@@ -20,12 +18,10 @@ setValue(urlParamsInst.code || PLAYGROUND_DEMO_CODE);
 </script>
 
 <template>
-  <div class="w-1/2 h-full py-2">
+  <div class="w-full min-w-0 py-2">
     <MonacoEditor
       ref="editorContainer"
       :language="language"
-      :width="width"
-      :height="height"
       @update:model-value="setValue"
       :value="editorValue"
     />
