65a029f9e301e0b2d7445bb2d9217ebab4791272
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-26 11:35:53 +0800

    refactor: responsive design

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -5,7 +5,7 @@ import Output from "./OutputPanel.vue";
 </script>
 
 <template>
-  <main class="flex flex-1 min-h-0">
+  <main class="flex flex-1 flex-col md:flex-row min-h-0">
     <Code :language="syntaxOptionState.language" />
     <div class="w-px h-full border-b bg-[#e2e2e3] dark:bg-[#2e2e32]" />
     <Output class="min-w-[50%]" />
