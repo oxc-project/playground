91ccd114365e393bcd5d01f1d5e345fb3e1f8bb4
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-27 15:34:57 +0800

    feat: options dialog

---
+++ src/components/Sidebar/OptionsDialog.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import SettingIcon from "~icons/ri/settings-line";
+import { useOxc } from "src/composables/useOxc";
+import { Button } from "src/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "src/ui/dialog";
+import { computed } from "vue";
+import Monaco from "../Editor/Monaco.vue";
+
+const { options } = await useOxc();
+const optionsString = computed({
+  get: () => JSON.stringify(options.value, null, 2),
+  set: (value: string) => {
+    try {
+      options.value = JSON.parse(value);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+});
+</script>
+
+<template>
+  <Dialog>
+    <DialogTrigger as-child>
+      <Button variant="outline" class="gap-1">
+        <SettingIcon />
+        Edit Options
+      </Button>
+    </DialogTrigger>
+
+    <DialogContent class="h-[80vh] max-w-5xl">
+      <DialogHeader>
+        <DialogTitle class="text-center">Customize Options</DialogTitle>
+      </DialogHeader>
+
+      <Monaco v-model="optionsString" language="json" />
+    </DialogContent>
+  </Dialog>
+</template>
--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { useOxc } from "src/composables/useOxc";
-import { computed } from "vue";
-import OutputPreview from "../Editor/Output/OutputPreview.vue";
 import Logo from "../Logo/Logo.vue";
 import Lint from "./Lint.vue";
+import OptionsDialog from "./OptionsDialog.vue";
 import Parse from "./Parse.vue";
-
-const { options } = await useOxc();
-const rawOptions = computed(() => JSON.stringify(options.value, null, 2));
 </script>
 
 <template>
@@ -18,10 +13,7 @@ const rawOptions = computed(() => JSON.stringify(options.value, null, 2));
     <div class="flex flex-col gap-6">
       <Parse />
       <Lint />
-      <details>
-        <summary>Raw options (dev)</summary>
-        <OutputPreview :code="rawOptions" lang="json" class="text-xs" />
-      </details>
+      <OptionsDialog />
     </div>
   </aside>
 </template>
