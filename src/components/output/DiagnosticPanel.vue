b00887475b53c077546dfe797d27f6bbf2168039
Boshen <boshenc@gmail.com>
2025-03-22 11:47:30 +0800

    feat: display diagnostic codeframe

--- src/components/output/DiagnosticPanel.vue
+++ src/components/output/DiagnosticPanel.vue
@@ -5,7 +5,11 @@ const { oxc } = await useOxc()
 </script>
 
 <template>
-  <div v-for="(diag, i) in oxc.getDiagnostics()" :key="i">
-    {{ diag.severity }}: {{ diag.message }}
+  <div
+    v-for="(diag, i) in oxc.getDiagnostics()"
+    :key="i"
+    style="white-space: pre"
+  >
+    {{ diag.codeframe }}
   </div>
 </template>
