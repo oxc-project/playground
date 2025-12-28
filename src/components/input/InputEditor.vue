36d6146bdb9f4cd8a4b64e7b6aa5cdbc9439a0e6
liangmiQwQ <github@liangmi.dev>
2025-12-28 19:48:31 +0800

    fix: watch oxc diagnostics changes rather than computing

--- src/components/input/InputEditor.vue
+++ src/components/input/InputEditor.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { templateRef } from '@vueuse/core'
 import * as monaco from 'monaco-editor'
-import { computed } from 'vue'
+import { computed, ref, watchEffect } from 'vue';
 import { useOxc } from '~/composables/oxc'
 import { editorValue } from '~/composables/state'
 import MonacoEditor from '../MonacoEditor.vue'
@@ -16,12 +16,13 @@ const { oxc } = await useOxc()
 
 const monacoRef = templateRef<InstanceType<typeof MonacoEditor>>('monacoRef')
 const getPositionAt = computed(() => monacoRef.value?.getPositionAt)
+const markers = ref<monaco.editor.IMarkerData[]>([]);
 
-const markers = computed((): monaco.editor.IMarkerData[] => {
+watchEffect(() => {
   if (!getPositionAt.value) return []
 
   const diagnostics = oxc.value.getDiagnostics()
-  return diagnostics.map((d) => {
+  markers.value = diagnostics.map((d) => {
     const startPos = getPositionAt.value(d.labels[0]?.start ?? 0)
     const endPos = getPositionAt.value(d.labels[0]?.end ?? 0)
     return {
