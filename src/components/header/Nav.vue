3e36858753c97b70487fe6772e6d74c50800d46e
Connor Shea <connor.james.shea@gmail.com>
2026-02-22 14:44:54 -0700

    Remove bad link to "Resources".

--- src/components/header/Nav.vue
+++ src/components/header/Nav.vue
@@ -72,22 +72,6 @@ import {
           <span class="text-sm text-[#0d6a73] font-medium dark:text-[#32F3E9]">Playground</span>
         </NavigationMenuLink>
       </NavigationMenuItem>
-
-      <NavigationMenuItem>
-        <NavigationMenuLink as-child>
-          <a class="flex items-center px-3" href="#" target="_blank" rel="noopener">
-            <span
-              class="text-sm text-[#16171d] font-medium dark:text-white hover:text-[#0d6a73] dark:hover:text-[#32F3E9]"
-            >
-              Resources
-            </span>
-            <Icon
-              icon="ri:arrow-right-up-line"
-              class="ml-1 h-3 w-3 text-[#827A89]"
-            />
-          </a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
     </NavigationMenuList>
   </NavigationMenu>
 </template>
